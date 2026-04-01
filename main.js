675b609a49046d542467c667c15e38d1f4636400
axelMtz123 <axelmartinezagju@icloud.com>
2026-03-31 20:37:47 -0700

    wokred on some more sections

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const split = new SplitType('.split', { types: 'chars' });
+
 /* -- h001-marquee -- */
 
-const track = document.querySelector(".marquee-track");
-const width = track.scrollWidth / 2;
+const h001Track = document.querySelector(".h001-marquee-track");
+const width = h001Track.scrollWidth / 2;
 
-const tl = gsap.to(track, {
+const tl = gsap.to(h001Track, {
   x: -width,
   duration: 20,
   ease: "none",
@@ -13,14 +15,42 @@ const tl = gsap.to(track, {
 });
 
 // pause on hover
-const marquee = document.querySelector(".h001-marquee");
+const h001Marquee = document.querySelector(".h001-marquee");
 
-marquee.addEventListener("mouseenter", () => {
+h001Marquee.addEventListener("mouseenter", () => {
   tl.pause();
 });
 
-marquee.addEventListener("mouseleave", () => {
+h001Marquee.addEventListener("mouseleave", () => {
   tl.resume();
 });
 
 /* -- h003 -- */
+
+const h003Track = document.querySelector(".h003-marquee-track");
+
+
+const totalHeight = h003Track.scrollHeight / 2;
+
+gsap.to(h003Track, {
+    y: -totalHeight,
+    duration: 32,
+    ease: "none",
+    repeat: -1,
+    modifiers: {
+        y: gsap.utils.unitize(y => parseFloat(y) % totalHeight)
+    }
+});
+
+/* -- ab001 -- */
+
+gsap.to('.ab001-rotate .char', {
+  rotation: 90,
+  stagger: 0.05,
+  scrollTrigger : {
+    trigger: '.ab001-rotate',
+    start: 'top 90%',
+    end: 'top 60%',
+    scrub: true,
+  }
+});
